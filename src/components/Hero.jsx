6aeab346346e4b067b11f4cb5ce7e71a0d990f7d
ropencorp <rts@ropencorp.com>
2025-06-29 19:39:31 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Bienvenid@ a <span className='text-[#915EFF]'>ROPENCORP</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Simplificamos tu experiencia Salesforce <br className='sm:block hidden' />
+            con atención Boutique
           </p>
         </div>
       </div>
