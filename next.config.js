fb601158da8310a3fefb4b772ee4b4baf57df6ae
Nahue <nahueretes@gmail.com>
2025-07-13 23:45:31 -0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // <--- Solo tienes que agregar esta línea
+};
 
 module.exports = nextConfig;
