10f0dc1feb5afd0558c348dda59cd37a7d9f0737
Nahue <nahueretes@gmail.com>
2025-07-13 22:46:37 -0300

    Reemplazo completo del proyecto con nuevo template estilo Snellenberg

---
+++ app/page.jsx
@@ -0,0 +1,31 @@
+import {
+  Contact,
+  Description,
+  Header,
+  Navbar,
+  Project,
+  Thumbnail,
+  Transition,
+} from '@/layout';
+
+/** @type {import('next').Metadata} */
+export const metadata = {
+  title: 'Home | Dennis Snellenberg',
+  description:
+    'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+};
+
+export default function Home() {
+  return (
+    <Transition>
+      <Navbar />
+      <Header />
+      <main>
+        <Description />
+        <Thumbnail />
+        <Project />
+      </main>
+      <Contact />
+    </Transition>
+  );
+}
