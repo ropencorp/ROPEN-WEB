c253a3c49b4a2d72265f2b4cb82ba8aa332c0202
Nahue <nahueretes@gmail.com>
2025-07-14 00:58:36 -0300

    new

--- app/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './common/button';
-export * from './in-progress';
-export * from './parallax';
-export * from './stack';
--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <--- Solo tienes que agregar esta línea
-};
-
-module.exports = nextConfig;
