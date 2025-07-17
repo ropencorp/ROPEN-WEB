ff35be36e199fad59bd160ccf0013c195e9403cc
Nahue <nahueretes@gmail.com>
2025-07-17 01:58:17 -0300

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
+  output: 'export',
+  images: {
+    unoptimized: true, // <-- AÑADE ESTAS LÍNEAS
+  },
 };
-
 export default nextConfig;
